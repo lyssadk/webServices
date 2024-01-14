e430572f468adbed0d3c2d77282f9893e22e5ae4
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-01-13 19:57:08 -0700

    port

--- lesson01/index.js
+++ lesson01/index.js
@@ -6,6 +6,6 @@ const routes = require("./routes/index");
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log('Server started on port ${port}');
+    console.log('Server started on port: ' + port);
 });
 
